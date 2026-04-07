9f30e5a4bacfcc72c58f5628934c6fb19b9543ad
Sandaru Anuththara <sandaruperera108@gmail.com>
2026-04-07 10:52:14 +0530

    feat(ui): implement profile screen using reusable custom button

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart'; 
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Profile"),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+           
+            const CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.indigo,
+              child: Icon(Icons.person, size: 60, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            
+            const Text(
+              "Sandaru", 
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
+            ),
+            const Text("sandaru@example.com", style: TextStyle(color: Colors.grey)),
+            
+            const Divider(height: 40), 
+            
+            
+            _buildProfileInfo(Icons.book, "Completed Courses", "08"),
+            _buildProfileInfo(Icons.assignment, "Assignments Done", "15"),
+            
+            const Spacer(), 
+
+            
+            CustomButton(
+              text: "EDIT PROFILE", 
+              onPressed: () {
+                print("Edit Profile Clicked!");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+ 
+  Widget _buildProfileInfo(IconData icon, String title, String value) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.indigo),
+      title: Text(title),
+      trailing: Text(value, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+    );
+  }
+}
