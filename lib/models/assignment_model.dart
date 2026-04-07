70d59c7c253babcd5e0603f4b5b650214059712c
Sandaru Anuththara <sandaruperera108@gmail.com>
2026-04-07 10:43:32 +0530

    feat(models): implement assignment data model

---
+++ lib/models/assignment_model.dart
@@ -0,0 +1,12 @@
+class Assignment {
+  final String title;
+  final String subject;
+  final String dueDate;
+  final bool isSubmitted; 
+  Assignment({
+    required this.title,
+    required this.subject,
+    required this.dueDate,
+    required this.isSubmitted,
+  });
+}
